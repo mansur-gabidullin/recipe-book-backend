73d4fad9d22b91633b72e27517cb3ff8ce95089a
Mansur Gabidullin <mansur.gabidullin@gmail.com>
2023-12-03 07:58:15 +0300

    Add dependencies.

--- recipe_book/dependencies.py
+++ recipe_book/dependencies.py
@@ -1,16 +1,16 @@
+from typing import AsyncIterator
 from uuid import UUID
 
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from settings import settings
 
 from constants import API_PREFIX
 
 from shared_kernal.interfaces.access_token_creator import ITokenCreator
 from shared_kernal.interfaces.password_hasher import IPasswordHasher
-from shared_kernal.access_token_creator import TokenCreator
+from shared_kernal.token_creator import TokenCreator
 from shared_kernal.password_hasher import PasswordHasher
 
 from application_core.auth.interfaces.auth_service import IAuthService
@@ -22,12 +22,14 @@ from application_core.bin.interfaces.bin_repository import IBinRepository
 from application_core.bin.interfaces.bin_service import IBinService
 from application_core.bin.services.bin_restore_service import BinService
 
+from application_core.products.interfaces.change_product_command import IChangeProductCommand
+from application_core.products.interfaces.product_query import IProductQuery
+from application_core.products.interfaces.remove_product_command import IRemoveProductCommand
 from application_core.products.interfaces.add_product_command import IAddProductCommand
 from application_core.products.interfaces.products_query import IProductsQuery
 from application_core.products.interfaces.products_repository import IProductsRepository
 from application_core.products.interfaces.products_service import IProductsService
 from application_core.products.interfaces.products_service_converter import IProductsServiceConverter
-from application_core.products.beans.add_product_command import AddProductCommand
 from application_core.products.converters.products_service_converter import ProductsServiceConverter
 from application_core.products.services.products_service import ProductsService
 
@@ -63,6 +65,8 @@ from infrastructure.session import AsyncScopedSession
 from presentation.interfaces.products.product_converter import IProductResponseConverter
 from presentation.interfaces.recipes.recipe_converter import IRecipeResponseConverter
 from presentation.interfaces.users.user_converter import IUserResponseConverter
+from presentation.interfaces.dictionary.dictionary_converter import IDictionaryResponseConverter
+from presentation.beans.products.add_product_command import AddProductCommand
 from presentation.beans.products.products_query import ProductsQuery
 from presentation.beans.recipes.recipes_query import RecipesQuery
 from presentation.beans.recipes.add_recipe_command import AddRecipeCommand
@@ -70,11 +74,40 @@ from presentation.beans.users.add_user_command import AddUserCommand
 from presentation.beans.users.remove_user_command import RemoveUserCommand
 from presentation.beans.bin.bin_command import BinCommand
 from presentation.beans.users.users_query import UsersQuery
+from presentation.beans.products.change_product_command import ChangeProductCommand
+from presentation.beans.products.product_query import ProductQuery
+from presentation.beans.products.remove_product_command import RemoveProductCommand
 from presentation.converters.products_response_converter import ProductResponseConverter
 from presentation.converters.recipes_response_converter import RecipeResponseConverter
 from presentation.converters.users_response_converter import UserResponseConverter
+from presentation.converters.dictionary_response_converter import DictionaryResponseConverter
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{API_PREFIX}/auth/token", auto_error=False)
+
+
+async def create_database_session() -> AsyncIterator[AsyncSession]:
+    async with AsyncScopedSession() as session:
+        async with session.begin():
+            try:
+                yield session
+            except Exception as error:
+                print(error)
+                await session.rollback()
+                raise error
+
+
+async def create_token_creator() -> ITokenCreator:
+    return TokenCreator(
+        secret_key=settings.token_secret_key,
+        algorithm=settings.token_algorithm,
+        csrf_expire_minutes=settings.csrf_token_expire_minutes,
+        access_expire_minutes=settings.access_token_expire_minutes,
+        refresh_expire_days=settings.refresh_token_expire_days,
+    )
+
+
+async def create_password_hasher() -> IPasswordHasher:
+    return PasswordHasher()
 
 
 async def create_users_query(login: str = None, limit: int = None) -> IUsersQuery:
@@ -101,22 +134,11 @@ async def create_user_repository_converter() -> IUserRepositoryConverter:
     return UserRepositoryConverter()
 
 
-async def create_database_session() -> AsyncSession:
-    async with AsyncScopedSession() as session:
-        async with session.begin():
-            yield session
-    await AsyncScopedSession.remove()
-
-
 async def create_users_repository(
     session: AsyncSession = Depends(create_database_session),
     converter: IUserRepositoryConverter = Depends(create_user_repository_converter),
 ) -> IUsersRepository:
     return UsersRepository(session, converter)
-
-
-async def create_password_hasher() -> IPasswordHasher:
-    return PasswordHasher()
 
 
 async def create_users_service(
@@ -125,16 +147,6 @@ async def create_users_service(
     password_hasher: IPasswordHasher = Depends(create_password_hasher),
 ) -> IUsersService:
     return UsersService(repository, converter, password_hasher)
-
-
-async def create_token_creator() -> ITokenCreator:
-    return TokenCreator(
-        secret_key=settings.token_secret_key,
-        algorithm=settings.token_algorithm,
-        csrf_expire_minutes=settings.csrf_token_expire_minutes,
-        access_expire_minutes=settings.access_token_expire_minutes,
-        refresh_expire_days=settings.refresh_token_expire_days,
-    )
 
 
 async def create_auth_service(
@@ -166,7 +178,7 @@ async def create_users_bin_query(login: str = None, limit: int = None) -> IUsers
 
 
 async def create_recipes_query(limit: int = None) -> IRecipesQuery:
-    return RecipesQuery(limit=limit, is_removed=True)
+    return RecipesQuery(limit=limit)
 
 
 async def create_recipes_response_converter() -> IRecipeResponseConverter:
@@ -200,7 +212,11 @@ async def create_add_recipe_command(addRecipeCommand: AddRecipeCommand) -> IAddR
 
 
 async def create_products_query(limit: int = None) -> IProductsQuery:
-    return ProductsQuery(limit=limit, is_removed=True)
+    return ProductsQuery(limit=limit)
+
+
+async def create_product_query(uuid: UUID) -> IProductQuery:
+    return ProductQuery(uuid=uuid)
 
 
 async def create_products_response_converter() -> IProductResponseConverter:
@@ -229,5 +245,17 @@ async def create_products_service(
     return ProductsService(repository, service_converter)
 
 
-async def create_add_product_command(addProductCommand: AddProductCommand) -> IAddProductCommand:
-    return addProductCommand
+async def create_add_product_command(command: AddProductCommand) -> IAddProductCommand:
+    return command
+
+
+async def create_change_product_command(command: ChangeProductCommand) -> IChangeProductCommand:
+    return command
+
+
+async def create_remove_product_command(uuid: UUID) -> IRemoveProductCommand:
+    return RemoveProductCommand(uuid=uuid)
+
+
+async def create_dictionary_response_converter() -> IDictionaryResponseConverter:
+    return DictionaryResponseConverter()
